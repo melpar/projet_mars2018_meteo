d1319735ec83982218d3ecb1193b8e083a4fdaa6
scawes <thomas.marescaux@hotmail.fr>
2018-03-13 09:36:48 +0100

    debug2

--- projet_mars2018_jsp/src/servlet/ServletEnvoyerDonnee.java
+++ projet_mars2018_jsp/src/servlet/ServletEnvoyerDonnee.java
@@ -26,58 +26,54 @@ import validation.Validation;
 @WebServlet("/ServletEnvoyerDonnee")
 public class ServletEnvoyerDonnee extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletEnvoyerDonnee() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletEnvoyerDonnee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+
 		int port = 2000;
-		
+
 		try {
-			Registry registry =
-					LocateRegistry.getRegistry(port);
-			
+			Registry registry = LocateRegistry.getRegistry(port);
+
 			Serveur serveur = (Serveur) registry.lookup("serveurRMI");
-			Validation valide = serveur.valider(
-					request.getParameter("pays"), 
-					request.getParameter("ville"), 
-					request.getParameter("departement"), 
-					request.getParameter("direction"), 
-					request.getParameter("vitesse"), 
-					request.getParameter("temperature")
-					);
-			if(valide.isValide()) {
-				//creation archive meteo
+			Validation valide = serveur.valider(request.getParameter("pays"), request.getParameter("ville"),
+					request.getParameter("departement"), request.getParameter("direction"),
+					request.getParameter("vitesse"), request.getParameter("temperature"));
+			if (valide.isValide()) {
+				// creation archive meteo
 				ArchiveMeteo archive = new ArchiveMeteo();
-				//recuperation date
+				// recuperation date
 				String dateString = request.getParameter("date");
-				SimpleDateFormat formatter = new SimpleDateFormat("dd MMM, yyyy",Locale.US);
+				SimpleDateFormat formatter = new SimpleDateFormat("dd MMM, yyyy", Locale.US);
 				Date date = new Date();
-		        try {
-		            date = formatter.parse(dateString);
-		        } catch (java.text.ParseException e) {
+				try {
+					date = formatter.parse(dateString);
+				} catch (java.text.ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				archive.setDate(date);
-				//recuperation du lieu
+				// recuperation du lieu
 				Lieu lieu = new Lieu();
 				lieu.setPays(request.getParameter("pays"));
 				lieu.setDepartement(request.getParameter("departement"));
 				lieu.setVille(request.getParameter("ville"));
 				archive.setLieu(lieu);
-				//recuperation donnee meteo
+				// recuperation donnee meteo
 				DonneeMeteo donnee = new DonneeMeteo();
 				donnee.setDirectionVent(Double.parseDouble(request.getParameter("direction")));
 				donnee.setPluie(Double.parseDouble(request.getParameter("pluie")));
@@ -85,27 +81,27 @@ public class ServletEnvoyerDonnee extends HttpServlet {
 				donnee.setTemperature(Integer.parseInt(request.getParameter("temperature")));
 				donnee.setVitesseVent(Double.parseDouble(request.getParameter("vitesse")));
 				archive.setDonnee(donnee);
-				//envois au serveur
+				// envois au serveur
 				serveur.ajouterDonneeArchive(archive);
 				System.out.println("passer archive");
 			} else {
 				request.setAttribute("valide", valide);
 				System.out.println("pas passer archive");
-				System.out.println(valide.getErreurs().get("direction"));
+				System.out.println(valide.getErreurs().get("temperature"));
+				System.out.println(valide.getValeurs().get("temperature"));
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Erreur client RMI" + e.toString());
 		}
-		request.getServletContext().getRequestDispatcher(
-				"/ajout.jsp").
-					forward(request, response);
+		request.getServletContext().getRequestDispatcher("/ajout.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
