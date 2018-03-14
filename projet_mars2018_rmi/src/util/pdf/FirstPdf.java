18f6a454c7fea0cf09ab09c19ff85ce953b26116
scawes <thomas.marescaux@hotmail.fr>
2018-03-14 16:42:05 +0100

    [pdf]

--- projet_mars2018_rmi/src/util/pdf/FirstPdf.java
+++ projet_mars2018_rmi/src/util/pdf/FirstPdf.java
@@ -7,11 +7,8 @@ import java.awt.image.RescaleOp;
 import java.awt.print.PageFormat;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.rmi.RemoteException;
 import java.util.Base64;
 import java.util.Date;
@@ -34,9 +31,6 @@ public class FirstPdf {
 
     public FirstPdf() {
 	p = new PDFDocument();
-
-	// p.setInfo("Author", "TeamDelArte");
-	// p.setInfo("Title", "Rapport météo");
     }
 
     public static void main(String[] args) throws RemoteException {
@@ -46,15 +40,7 @@ public class FirstPdf {
 	List<ArchiveMeteo> list = serveur.consulterParMois(date);
 	ArchiveMeteo archive = list.get(0);
 	FirstPdf pdf = new FirstPdf();
-	/*
-	 * OutputStream fo = pdf.download(); pdf.creer(fo); try { fo.close(); } catch
-	 * (FileNotFoundException e) { // TODO Auto-generated catch block
-	 * e.printStackTrace(); } catch (IOException e) { // TODO Auto-generated catch
-	 * block e.printStackTrace(); }
-	 */
-
 	try {
-	    // Create a document and a page in default Locale format
 	    pdf.nouvellePage(archive);
 	    pdf.p.saveDocument("Rapport.pdf");
 	    pdf.generer();
@@ -71,7 +57,6 @@ public class FirstPdf {
 
     public void nouvellePage(ArchiveMeteo archive) {
 	PDFPage newPage = p.createPage(new PageFormat());
-	int hauteurPage = (int) newPage.getDocument().getDefaultPageFormat().getHeight();
 	int marge = 100;
 	// Draw to the page
 	Graphics2D g2d = newPage.createGraphics();
@@ -103,24 +88,11 @@ public class FirstPdf {
 		byte[] photoDecode = Base64.getDecoder().decode(photo);
 		InputStream in = new ByteArrayInputStream(photoDecode);
 		try {
-		    // image = new PDFImage(in);
-		    // float hauteur = image.getHeight() * 100 / image.getHeight();
-		    // float longueur = image.getWidth() * 100 / image.getHeight();
-		    // float x1 = 100;
-		    // float x2 = x1 + longueur;
-		    // float y1 = 100 * i;
-		    // float y2 = y1 - hauteur;
-
-		    // g2d.drawImage(imag, x1, y1, x2, y2);
 		    float[] scales = { 1f, 1f, 1f, 1f };
 		    float[] offsets = new float[4];
 		    RescaleOp rop = new RescaleOp(scales, offsets, null);
-
-		    /* Draw the image, applying the filter */
 		    g2d.drawImage(ajouterImage(in), rop, 100, 100 + (200 * i));
-
 		} catch (IOException e) {
-		    // TODO Auto-generated catch block
 		    e.printStackTrace();
 		}
 		list.remove(0);
@@ -130,31 +102,13 @@ public class FirstPdf {
     }
 
     private BufferedImage ajouterImage(InputStream in) throws IOException {
-	/* Create an ARGB BufferedImage */
 	BufferedImage img = ImageIO.read(in);
-	// int hauteur = img.getHeight(null) * 100 / img.getHeight(null);
-	// int longueur = img.getHeight(null) * 100 / img.getHeight(null);
 	int w = img.getWidth(null);
 	int h = img.getHeight(null);
 	BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 	Graphics g = bi.getGraphics();
 	g.drawImage(img, 0, 0, null);
 	return bi;
-    }
-
-    public OutputStream download() {
-	OutputStream fo;
-	try {
-	    fo = new FileOutputStream("Rapport.pdf");
-	    return fo;
-	} catch (FileNotFoundException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	return null;
     }
 
     public byte[] generer() {
@@ -164,7 +118,6 @@ public class FirstPdf {
 	    byte[] fichier = os.toByteArray();
 	    return fichier;
 	} catch (IOException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	return null;
